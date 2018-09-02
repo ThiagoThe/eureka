30f6f777f90e3d7de4f4cce73d5d90fede3fd1e9
Thiago Pires <nitrox2thi@gmail.com>
2018-09-02 19:26:48 -0300

    Evlot

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+
+
+//task for sass
+gulp.task('sass', function() {
+    return gulp.src('sass/**/*.sass')
+        .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('css'));
+});
+
+//task for watch
+gulp.task('sass:watch', function() {
+    gulp.watch('sass/**/*.sass',['sass']);
+});
+
+
